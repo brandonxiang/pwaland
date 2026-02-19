641fac1d6e6c0c5ef9fb6f37e0d5c276d20fbb66
brandon.xiang <1542453460@qq.com>
2026-02-19 21:49:26 +0800

    chore: rename project to pwaland-web-next, add module type to package.json, and document Vercel entry point in API

--- projects/web-next/api/index.js
+++ projects/web-next/api/index.js
@@ -11,6 +11,15 @@ const app = Fastify({
 // 将应用注册为一个常规插件
 app.register(import("../../server/dist/plugin.mjs"));
 
+/**
+ * Vercel 入口文件
+ * @param {Request} req - Vercel 请求对象
+ * @param {import('http').ServerResponse} res - Vercel 响应对象
+ * @returns {Promise<void>}
+ * @example
+ * import { handler } from './api/index.js';
+ * export const handler = handler;
+ */
 export default async (req, res) => {
   await app.ready();
   app.server.emit('request', req, res);
