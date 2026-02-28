96bbe1cc34b1b8523552cde7bb2d130c5bd8ea62
brandon.xiang <1542453460@qq.com>
2026-02-28 11:23:08 +0800

    chore: update .gitignore to include .env*.local, remove pnpm-lock.yaml, and modify Vercel configuration for build commands and rewrites

--- projects/web-next/api/index.js
+++ projects/web-next/api/index.js
@@ -1,26 +1,12 @@
-"use strict";
-
-// 引入 Fastify 框架
 import Fastify from "fastify";
 
-// 实例化 Fastify
 const app = Fastify({
   logger: true,
 });
 
-// 将应用注册为一个常规插件
 app.register(import("../../server/dist/plugin.mjs"));
 
-/**
- * Vercel 入口文件
- * @param {Request} req - Vercel 请求对象
- * @param {import('http').ServerResponse} res - Vercel 响应对象
- * @returns {Promise<void>}
- * @example
- * import { handler } from './api/index.js';
- * export const handler = handler;
- */
 export default async (req, res) => {
   await app.ready();
-  app.server.emit('request', req, res);
-}
+  app.server.emit("request", req, res);
+};
